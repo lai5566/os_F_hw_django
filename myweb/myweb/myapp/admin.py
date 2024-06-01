9dc441aad17c27f1b89e49066bfbcdb097eae187
lai5566 <69065658+lai5566@users.noreply.github.com>
2024-06-01 20:12:43 +0800

    add register_finish.html'

--- myweb/myweb/myapp/admin.py
+++ myweb/myweb/myapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from myapp import models
 # Register your models here.
+from .models import BloodPressureEntry
 
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display=('userID','created')
-
 admin.site.register(models.UserProfile,UserProfileAdmin)
+
+
+admin.site.register(BloodPressureEntry)
